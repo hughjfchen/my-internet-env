505b8ed8a744dce18f35f3cd0ae8be74e8b89e76
hughjfchen <hugh.jf.chen@gmail.com>
2020-01-15 21:29:19 +0800

    add ss

---
+++ ss/DevOps/gcp-hk/test/ct.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+if ! type dirname > /dev/null 2>&1; then
+    echo "Not even a linux or macOS, Windoze? We don't support it. Abort."
+    exit 1
+fi
+
+. "$(dirname "$0")"/../../common/common.sh
+
+init_with_root_or_sudo "$0"
+
+begin_banner "ss" "Cont. Test"
+
+${SCRIPT_ABS_PATH}/../../../../DevOps/gcp-hk/test/prepare.sh
+${SCRIPT_ABS_PATH}/../../../../DevOps/gcp-hk/test/test/prepare.sh
+
+${SCRIPT_ABS_PATH}/prepare.sh
+${SCRIPT_ABS_PATH}/test/prepare.sh
+${SCRIPT_ABS_PATH}/test/test.sh
+${SCRIPT_ABS_PATH}/test/finishing.sh
+${SCRIPT_ABS_PATH}/finishing.sh
+
+${SCRIPT_ABS_PATH}/../../../../DevOps/gcp-hk/test/test/finishing.sh
+${SCRIPT_ABS_PATH}/../../../../DevOps/gcp-hk/test/finishing.sh
+
+done_banner "ss" "Cont. Test"
---
+++ ss/DevOps/gcp-hk/test/deploy/deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+if ! type dirname > /dev/null 2>&1; then
+    echo "Not even a linux or macOS, Windoze? We don't support it. Abort."
+    exit 1
+fi
+
+. "$(dirname "$0")"/../../../common/common.sh
+
+init_with_root_or_sudo "$0"
+
+begin_banner "ss" "deploy deploy"
+
+docker run -dt --rm --name ssserver-kcptun -p 6443:6443 -p 6500:6500/udp mritd/shadowsocks -m "ss-server" -s "-s 0.0.0.0 -p 6443 -m chacha20-ietf -k Passw0rd" -x -e "kcpserver" -k "-t 127.0.0.1:6443 -l :6500 -mode fast2"
+docker run -dt --rm --name ssserver-obfs -p 8443:8443  mritd/shadowsocks -m "ss-server" -s "-s 0.0.0.0 -p 8443 -m chacha20-ietf -k Passw0rd --plugin obfs-server --plugin-opts obfs=tls"
+
+done_banner "ss" "deploy deploy"
